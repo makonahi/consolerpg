32252a3e207b7eeef4059cd2900146acfefe34c9
Matvey KIrpichev Alekseevich <kirpichev.matvei@yandex.ru>
2024-09-23 23:02:01 +0300

    Working Build v0.1.23092024

--- src/Program.java
+++ src/Program.java
@@ -3,7 +3,7 @@ import Functionalities.GameStarter.GameProcessor;
 
 public class Program {
     public static void main(String[] args) {
-        GameProcessor.startNewGame();
+            GameProcessor.startNewGame();
     }
 
 }
